a55027a2da66a83cbb2ee1387c7097a86d5ff458
ignacio <ignaciomenchaca666@gmail.com>
2020-04-22 17:28:52 -0500

    new icons

--- src/components/WeatherComponent.jsx
+++ src/components/WeatherComponent.jsx
@@ -1,92 +1,58 @@
 import React, {useEffect, useState} from 'react'
+import {appId, apiKeyAlgolia, apiKey} from '../environment';
 import AlgoliaPlaces from 'algolia-places-react';
-import {apiKey, appId, apiKeyAlgolia} from '../environment';
+import CurrentWeatherComponent from "./CurrentWeatherComponent";
+import ErrorMessageComponent from "./ErrorMessageComponent";
 
 function WeatherComponent() {
-	let [error, setError] = useState('')
-	
-	let [currentTemperature, setCurrentTemperature] = useState({
+	let [error, setError] = useState(false)
+	let [currentWeather, setCurrentWeather] = useState({
 		temp: '1',
 		feels_like: '-1',
 		description: 'snowing',
-		icon: '13n'
-	})
-	
-	let [location, setLocation] = useState({
+		icon: '50d',
 		name: 'Tala, MX',
 		lat: 20.67,
 		lon: -103.7
 	});
 	
-	let [url, setUrl] = useState(`https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${apiKey}&units=metric`)
+	let [url, setUrl] = useState(`https://api.openweathermap.org/data/2.5/weather?lat=${currentWeather.lat}&lon=${currentWeather.lon}&appid=${apiKey}&units=metric`);
 	
 	useEffect(() => {
 		const getWeather = async () => {
 			try {
 				let res = await fetch(url);
 				res = await res.json();
-				
 				let {description, icon} = res.weather[0];
 				let {temp, feels_like} = res.main;
-				
-				setCurrentTemperature({temp, feels_like, description, icon});
-				setLocation({name: `${res.name}, ${res.sys.country}`, lat: res.coord.lat, lon: res.coord.lon});
+				setCurrentWeather({
+					temp,
+					feels_like,
+					description,
+					icon,
+					name: `${res.name}, ${res.sys.country}`,
+					lat: res.coord.lat,
+					lon: res.coord.lon
+				});
 			} catch (e) {
-				setError('this account is temporary blocked due to exceeding of requests limitation of your subscription type');
+				setError(true);
 			}
 		};
-		
 		getWeather();
-	}, [url]);
+	}, [url])
 	
 	return (
 		<div className="text-white mt-32">
-			{error &&
-			<div className="bg-red-100 border-t-4 md:w-128 border-red-500 rounded-b text-red-900 px-4 py-3 shadow-md"
-					 role="alert">
-				<div className="flex">
-					<div className="py-1">
-						<svg className="fill-current h-6 w-6 text-red-500 mr-4" xmlns="http://www.w3.org/2000/svg"
-								 viewBox="0 0 20 20">
-							<path
-								d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/>
-						</svg>
-					</div>
-					<div>
-						<p className="font-bold">Something seriously bad happened.</p>
-						<p className="text-sm">{error}.</p>
-					</div>
-				</div>
-			</div>
-			}
+			{error && <ErrorMessageComponent/>}
 			<div className="places-input text-gray-800 mt-5">
 				<AlgoliaPlaces
 					placeholder='Write an address here'
-					options={{
-						appId: appId,
-						apiKey: apiKeyAlgolia,
-						aroundLatLngViaIP: false
-					}}
 					onChange={({suggestion}) => setUrl(`https://api.openweathermap.org/data/2.5/weather?lat=${suggestion.latlng.lat}&lon=${suggestion.latlng.lng}&appid=${apiKey}&units=metric`)}
+					options={{appId, apiKey: apiKeyAlgolia, aroundLatLngViaIP: false}}
 				/>
 			</div>
-			<div
-				className="container mx-auto font-sans md:w-128 max-w-lg rounded-lg overflow-hidden bg-gray-900 shadow-lg mt-8">
-				<div className="current-weather flex items-center justify-between px-6 py-8">
-					<div className="flex flex-col md:flex-row items-center">
-						<div>
-							<div className="text-6xl font-semibold">{currentTemperature.temp}°C</div>
-							<div>Feels like {currentTemperature.feels_like}°C</div>
-						</div>
-						<div className="md:mx-5">
-							<div className="font-semibold">{currentTemperature.description}</div>
-							<div>{location.name}</div>
-						</div>
-					</div>
-					<div>
-						<img src={`http://openweathermap.org/img/wn/${currentTemperature.icon}.png`} width="60" height="60" alt=""/>
-					</div>
-				</div>
+			<div className="container mx-auto font-sans md:w-128 max-w-lg rounded-lg overflow-hidden bg-gray-900 shadow-lg mt-8">
+				<CurrentWeatherComponent weather={currentWeather}/>
 			</div>
 		</div>
 	);
--- src/environment.js
+++ src/environment.js
@@ -1,11 +1,5 @@
 // open weather key
-const apiKey = '910223db7244fe5348d517710ba36961';
+export const apiKey = '910223db7244fe5348d517710ba36961';
 // algolia places keys
-const appId = 'plQ0HEAREVSZ';
-const apiKeyAlgolia = 'adce344ab26b5c12d2362a2f6bc7e06e';
-
-export {
-	apiKey,
-	appId,
-	apiKeyAlgolia
-}
+export const appId = 'plQ0HEAREVSZ';
+export const apiKeyAlgolia = 'adce344ab26b5c12d2362a2f6bc7e06e';
